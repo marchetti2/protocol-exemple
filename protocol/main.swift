30729c493c1db75cb4296c4df1ecc1c483510bc1
Mário Alves <mario.alves@superopa.com>
2023-01-02 12:23:45 -0300

    Up to github

--- protocol/main.swift
+++ protocol/main.swift
@@ -1,11 +1,54 @@
-//
-//  main.swift
-//  protocol
-//
-//  Created by Eydde on 02/01/23.
-//
+protocol CanFly {
+    func fly()
+    
+}
 
-import Foundation
 
-print("Hello, World!")
+class Bird {
+    var isFemale = true
+    
+    func layEgg() {
+        if isFemale {
+            print("The bird make a new bird in a shell")
+        }
+    }
+}
 
+
+class Eagle: Bird, CanFly {
+    func fly() {
+        print("The eagle flpas its wings and lift off into the sky.")
+    }
+    
+    func soar() {
+        print("The eagle glides in the air using air currents")
+    }
+}
+
+class Penguin: Bird {
+    func swim() {
+        print("The pinguin paddles through the water.")
+    }
+    
+}
+
+struct FlyingMuseum {
+    func flyingDemo(flyingObject: CanFly) {
+        flyingObject.fly()
+    }
+}
+
+
+struct AirPlane: CanFly {
+    func fly() {
+        print("the airplane uses its engine to lift off into the air")
+    }
+    
+}
+
+let myEagle = Eagle()
+let penguin = Penguin()
+let myPlane = AirPlane()
+
+let museum = FlyingMuseum()
+museum.flyingDemo(flyingObject: myPlane)
